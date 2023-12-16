4a53e939f3eabc29dbbab81b74d2e7af7aad0b8e
shahabas <mohdshahabasm@gmail.com>
2023-12-16 14:44:59 +0530

    data ingestion outline updated

---
+++ src/CNN_classifier/components/stage_01_data_ingestion.py
@@ -0,0 +1,25 @@
+import os
+import urllib.request as request
+from zipfile import ZipFile
+from CNN_classifier import logger
+from tqdm import tqdm
+from pathlib import Path
+from CNN_classifier.entity import DataIngestionConfig
+from CNN_classifier.utils import utils
+
+
+class DataIngestion:
+    def __init__(self):
+        pass
+    
+    def download_data(self):
+        pass
+    
+    def get_updated_list_of_files(self):
+        pass
+    
+    def preprocess(self):
+        pass
+
+    def unzip_and_clean(self):
+        pass
