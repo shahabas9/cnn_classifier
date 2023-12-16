f264c201777a2f6890a833ad44376d7fd76f68eb
shahabas <mohdshahabasm@gmail.com>
2023-12-16 14:43:47 +0530

    logger added

---
+++ src/CNN_classifier/__init__.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import logging
+import time
+
+log_dir="logs"
+log_path=os.path.join(log_dir,f"log_{time.time()}.log")
+os.makedirs(log_dir,exist_ok=True)
+logging_str="[%(ascstime)s:%(levelname)s:%(module)s]:%(message)s"
+logging.basicConfig(
+    level=logging.INFO,
+    format=logging_str,
+    handlers=[
+        logging.FileHandler(log_path),
+        logging.Streamhandler(sys.stdout)
+    ]
+)
+logger=logging.getLogger("cnn_classifier")
