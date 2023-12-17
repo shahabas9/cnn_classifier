f479cb8b3ffdaabe7166bcd32cfd7d833467dac8
shahabas <mohdshahabasm@gmail.com>
2023-12-17 12:25:40 +0530

    data ingestion structural hierarchy updated

--- src/CNN_classifier/pipeline/stage_01_data_ingestion.py
+++ src/CNN_classifier/pipeline/stage_01_data_ingestion.py
@@ -1,9 +1,15 @@
 from CNN_classifier.components.stage_01_data_ingestion import DataIngestion
 import os 
-from CNN_classifier.config import ConfigurationManager
+from CNN_classifier.config.configuaration import ConfigurationManager
 from CNN_classifier import logger
+
+logger.info(f"data ingestion stage starting")
 
 config=ConfigurationManager()
 data_ingestion_config=config.get_data_ingestion_config()
 
-data_ingestion=DataIngestion()
+data_ingestion=DataIngestion(config=data_ingestion_config)
+data_ingestion.download_data()
+data_ingestion.unzip_and_clean()
+
+logger.info(f"data ingestion stage completed sucessfully")
