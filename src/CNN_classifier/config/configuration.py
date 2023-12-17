e5d9ab6c7947b1bd7978f06ca319135dfe44f932
shahabas <mohdshahabasm@gmail.com>
2023-12-17 12:24:35 +0530

    configuation in config repo added

---
+++ src/CNN_classifier/config/configuration.py
@@ -0,0 +1,13 @@
+from CNN_classifier.utils.utils import read_yaml,create_directory
+from CNN_classifier.entity.config_entity import DataIngestionConfig
+from CNN_classifier.constants import CONFIG_FILE_PATH,PARAMS_FILE_PATH
+import os
+from pathlib import Path
+
+class ConfigurationManager:
+
+    def __init__():
+        pass
+
+    def get_data_ingestion_config():
+        pass
